e4a5776a58f2e2cab9eff3b47a2854e99b6802b4
ejidike esther <ejidikeesther@gmail.com>
2022-04-26 19:38:17 +0100

    Edit Metadata

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Check, PasswordChecker } from 'react-password-strengthbar-ui';
+import { Strength, PasswordChecker } from 'react-password-strengthbar-ui';
 import { useState } from 'react';
 import './App.css';
 
@@ -14,8 +14,8 @@ function App() {
         console.log(data)
     }
 
-    let result = Check('password')
-    console.log(result)
+    let result = Strength('password')
+    //console.log(result)
     
 
     return (
@@ -24,7 +24,7 @@ function App() {
                 <div className='w-80 md:w-3/5 md:max-w-lg'>
                     <h1 className='text-sky-900 text-center text-xl md:text-3xl mb-5'>@react-password-strengthbar-ui</h1>
                     <input onChange={handlepassword} className='w-full rounded-md shadow py-2 px-4 focus:outline-sky-400'/>
-                    <PasswordChecker password={password} checkerData={getData}/>
+                    <PasswordChecker password={password} strengthData={getData}/>
                 </div>
             </div>
         </>
